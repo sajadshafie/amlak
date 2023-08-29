4be3ece977860f3ed190430dee9d333ac8a19c9b
sajad <sajad@sajads-Mac-mini.local>
2023-08-29 07:27:11 -0700

    add produc

---
+++ components/main/product/Items.ts
@@ -0,0 +1,15 @@
+import productType from "@/types/product";
+
+export const product_item: productType[] = [
+  {
+    title: "",
+    city: "",
+    meter: 300,
+    image: "",
+    description: "",
+    type: "",
+    id: 1,
+    introduction_code: 21235,
+    price: "",
+  },
+];
---
+++ types/product.ts
@@ -0,0 +1,13 @@
+interface productType {
+  title: string;
+  city: string;
+  meter: string;
+  image: string;
+  description: string;
+  type: string;
+  id: number;
+  introduction_code: number;
+  price: string;
+}
+
+export default productType;
